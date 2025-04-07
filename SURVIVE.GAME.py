4b257a565f8828f5421cf7226535497ee02b4875
htook <htook@berea.edu>
2025-04-07 19:12:43 -0400

    created the game run event

---
+++ SURVIVE.GAME.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Game:
+    def __init__(self):
+        self.size = 800, 600
+        self.running = True
+        pygame.init()
+        self.screen = pygame.display.set_mode(self.size)
+        self.screen.fill('#9CBEBA')
+        self.clock = pygame.time.Clock()
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+
+def main():
+    game = Game()
+    game.run()
+main()
