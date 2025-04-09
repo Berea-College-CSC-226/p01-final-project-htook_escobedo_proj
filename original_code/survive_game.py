b99e47571ffce661fdca6988fd2ace7f091d8abe
htook <htook@berea.edu>
2025-04-09 10:06:47 -0400

    working on time and score

--- original_code/survive_game.py
+++ original_code/survive_game.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 class Game:
     """
@@ -11,8 +12,9 @@ This is the setup for the screen
         self.running = True
         pygame.init()
         self.screen = pygame.display.set_mode(self.size)
-
+        self.font = pygame.font.SysFont(None, 40)
         self.clock = pygame.time.Clock()
+        self.start_time = pygame.time.get_ticks()
 
     def run(self):
 
@@ -20,11 +22,16 @@ This is the setup for the screen
 
         This will run the actual code.
         """
+
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
             self.screen.fill('#9CBEBA')
+            seconds = (pygame.time.get_ticks() - self.start_time) / 1000
+            # self.screen.fill((60, 20, 30))
+            timer_text = self.font.render(f"Score: {seconds:.2f}s", True, (255, 255, 255))
+            self.screen.blit(timer_text, (0, 0))
             pygame.display.flip()
             self.clock.tick(60)
 
@@ -44,9 +51,7 @@ This is the setup for the screen
 
 
 
-    def time(self):
-     """This counts the time until death function is triggered"""
-    pass
+
     def scoreboard(self):
         """This will use the time funtion to get the score, everytime time goes up
         so does the score. """
