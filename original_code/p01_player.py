370eedd98bd12b1d36fdc06fcda7cd67cc5ce7f7
escobedos <escobedos@berea.edu>
2025-04-28 04:59:38 -0400

    pulled an all nighter- 1. sized down the sprites 2. intergrated maze 3. No collision on maze 4. sized up the maze to computer screen 5. designed layout of maze 6. changed cosmetics for GUI; used minecraft button but it doesnt have to stay like that 7. Just finish making the cosmetics game runs perfectly

--- original_code/p01_player.py
+++ original_code/p01_player.py
@@ -2,7 +2,7 @@
 
 import pygame
 import random
-"hello2"
+
 class Entity(pygame.sprite.Sprite):
 
     """This class represents the entity on the screen"""
@@ -14,28 +14,35 @@ class Entity(pygame.sprite.Sprite):
         super().__init__()
         self.screen_size = screen_size
         print("Game Starting")
-        self.surf = pygame.image.load('Player.PNG').convert_alpha()
-        self.surf.set_colorkey((255, 255, 255), pygame.RLEACCEL)
-        self.rect = self.surf.get_rect()
 
-
-    def p1movement(self, keys):
-        """
-        This def makes the user able to control the player sprite with the keyboard keys.
-
-        Handles up, down, left, right movement events from the user
-
-        :param keys: key presses from pygame event listener
-        :return: None
-        """
-        if keys[pygame.K_UP]:
-            self.rect.move_ip(0, -3)
-        elif keys[pygame.K_DOWN]:
-            self.rect.move_ip(0, 3)
-        if keys[pygame.K_RIGHT]:
-            self.rect.move_ip(3, 0)
-        elif keys[pygame.K_LEFT]:
-            self.rect.move_ip(-3, 0)
+    #
+    # def p1movement(self, walls):
+    #     """
+    #     This def makes the user able to control the player sprite with the keyboard keys.
+    #
+    #     Handles up, down, left, right movement events from the user
+    #
+    #     :param keys: key presses from pygame event listener
+    #     :return: None
+    #     """
+    #     keys = pygame.key.get_pressed()
+    #     dx = dy = 0
+    #     if keys[pygame.K_UP]:
+    #         self.rect.move_ip(0, -3)
+    #     elif keys[pygame.K_DOWN]:
+    #         self.rect.move_ip(0, 3)
+    #     if keys[pygame.K_RIGHT]:
+    #         self.rect.move_ip(3, 0)
+    #     elif keys[pygame.K_LEFT]:
+    #         self.rect.move_ip(-3, 0)
+    #
+    #     self.rect.x += dx
+    #     if pygame.sprite.spritecollideany(self, walls):
+    #             self.rect.x -= dx
+    #
+    #     self.rect.y += dy
+    #     if pygame.sprite.spritecollideany(self, walls):
+    #             self.rect.y -= dy
 
 class Mob(Entity):
     """This will differinciate the NPC on the screen which moves randombly
@@ -43,12 +50,13 @@ class Mob(Entity):
     def __init__(self, screen_size):
         super().__init__(screen_size)
         self.surf = pygame.image.load('Shocked.mob.png').convert_alpha()
+        self.surf = pygame.transform.scale(self.surf, (35, 35))
         self.surf.set_colorkey((255, 255, 255), pygame.RLEACCEL)
         self.rect = self.surf.get_rect()
         self.rect.move_ip(self.screen_size[0]*3//4, self.screen_size[1]*3//4)
-        self.move_distance = 20
+        self.move_distance = 4
         self.directions = ["north", "east", "west", "south"]
-        self.path = random.choices(self.directions)
+        self.path = random.choice(self.directions)
         # Entity.position = [100, 100]
     def npc_directions(self):
 
@@ -68,17 +76,29 @@ class Mob(Entity):
             # Randomly change direction 5% of the time
             self.path = random.choice(self.directions)
 
-    def npcmovement(self,):
+    def npcmovement(self, walls):
         """This is the movement for the npc which makes it randomly use different movements
         to reach the player. """
-        if self.path == "north":
-            self.rect.move_ip(0, -self.move_distance)
-        elif self.path == "south":
-            self.rect.move_ip(0, self.move_distance)
-        elif self.path == "east":
-            self.rect.move_ip(self.move_distance, 0)
-        elif self.path == "west":
-            self.rect.move_ip(-self.move_distance, 0)
+        move = {
+            "north": (0, -self.move_distance),
+            "south": (0, self.move_distance),
+            "east": (self.move_distance, 0),
+            "west": (-self.move_distance, 0)
+        }
+
+        dx, dy = move[self.path]
+
+        # Try moving
+        self.rect.x += dx
+        if pygame.sprite.spritecollideany(self, walls):
+            self.rect.x -= dx
+            self.path = random.choice(self.directions)  # try new direction
+            return
+
+        self.rect.y += dy
+        if pygame.sprite.spritecollideany(self, walls):
+            self.rect.y -= dy
+            self.path = random.choice(self.directions)
 
 
 
@@ -86,30 +106,68 @@ class Player(Entity):
     def __init__(self, screen_size):
         super().__init__(screen_size)
         self.surf = pygame.image.load('Player.PNG').convert_alpha()
+        self.surf = pygame.transform.scale(self.surf, (35, 35))
         self.surf.set_colorkey((255, 255, 255), pygame.RLEACCEL)
         self.rect = self.surf.get_rect()
-        self.rect.move_ip(self.screen_size[0]//4, self.screen_size[1]//4)
+        self.rect.move_ip(self.screen_size[0]*1//4, self.screen_size[1]*1//4)
         # Entity.position = [0, 0]
 
 
+    def p1movement(self, walls):
+        """
+        This def makes the user able to control the player sprite with the keyboard keys.
 
-    def npcmovement(self,):
-        """This is the movement for the npc which makes it randomly use different movements
-        to reach the player. """
-        if self.path == "north":
-            self.rect.move_ip(0, -self.move_distance)
-            self.position[1] -= self.move_distance
-        elif self.path == "south":
-            self.rect.move_ip(0, self.move_distance)
-            self.position[1] += self.move_distance
-        if self.path == "east":
-            self.rect.move_ip(self.move_distance, 0)
-            self.position[0] -= self.move_distance
-        if self.path == "west":
-            self.rect.move_ip(-self.move_distance, 0)
-            self.position[0] += self.move_distance
+        Handles up, down, left, right movement events from the user
 
-        self.npc_directions()
+        :param keys: key presses from pygame event listener
+        :return: None
+        """
+        dx = dy = 0
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            dx = -3
+        elif keys[pygame.K_RIGHT]:
+            dx = 3
+        if keys[pygame.K_UP]:
+            dy = -3
+        elif keys[pygame.K_DOWN]:
+            dy = 3
+
+        self.rect.x += dx
+        if pygame.sprite.spritecollideany(self, walls):
+                self.rect.x -= dx
+
+        self.rect.y += dy
+        if pygame.sprite.spritecollideany(self, walls):
+                self.rect.y -= dy
+
+
+class Maze(pygame.sprite.Sprite):
+    def __init__(self, x, y, w, h):
+        super().__init__()
+        self.image = pygame.Surface((w, h))
+        self.image.fill((255, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    #
+    # def npcmovement(self,):
+    #     """This is the movement for the npc which makes it randomly use different movements
+    #     to reach the player. """
+    #     if self.path == "north":
+    #         self.rect.move_ip(0, -self.move_distance)
+    #         self.position[1] -= self.move_distance
+    #     elif self.path == "south":
+    #         self.rect.move_ip(0, self.move_distance)
+    #         self.position[1] += self.move_distance
+    #     if self.path == "east":
+    #         self.rect.move_ip(self.move_distance, 0)
+    #         self.position[0] -= self.move_distance
+    #     if self.path == "west":
+    #         self.rect.move_ip(-self.move_distance, 0)
+    #         self.position[0] += self.move_distance
+    #
+    #     self.npc_directions()
 
 #
 # class Player(Entity):
--- original_code/survive_game.py
+++ original_code/survive_game.py
@@ -1,6 +1,6 @@
 
 import pygame
-from p01_player import Entity, Player, Mob
+from p01_player import Entity, Player, Mob, Maze
 import sys
 import tkinter as tk
 
@@ -19,19 +19,56 @@ This is the setup for the screen
         self.clock = pygame.time.Clock()
         self.start_time = pygame.time.get_ticks()
         self.screen.fill('#9CBEBA')
+
+        #Creates Player and NPC
         self.guy = Player(self.size)
         self.badguy = Mob(self.size)
+
+        tile_size = 39
+        screen_width, screen_height = self.size
+        columns = screen_width // tile_size
+        rows = screen_height // tile_size
+
+        self.maze_layout = [
+            "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",  # row 1
+            "X              XX              X",  # row 2
+            "X X XXX XXX XX  X X  XXX XXX X X",  # row 3
+            "X X     X    XX   XX   X     X X",  # row 4
+            "X XX XX   XX X  X XXXX XX X XX X",  # row 5
+            "X     X   X    XX X    X  X  X X",  # row 6
+            "X XXX   X   XXXX    X XX XXX   X",  # row 7
+            "X   XXX XXXXX    X XX        X X",  # row 8
+            "X X      X    XX X    XXXX XXX X",  # row 9
+            "X XXX  X    X  X X XX  X    X  X",  # row 10
+            "X   XX XXX XXX      XX   XX    X",  # row 11
+            "X X      X     XX X    X X   X X",  # row 12
+            "X X XX X XXX X    XXX XX X XXX X",  # row 13
+            "X X    X     XX X          X   X",  # row 14
+            "X XXX XXXX XXXX XXX XXXXXX X X X",  # row 15
+            "X                              X",  # row 16
+            "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",  # row 17
+        ]
+
+        self.walls = pygame.sprite.Group()
+        for row_idx, row in enumerate(self.maze_layout):
+            for col_idx, tile in enumerate(row):
+                if tile == "X":
+                    x = col_idx * tile_size
+                    y = row_idx * tile_size
+                    self.walls.add(Maze(x, y, tile_size, tile_size))
+
 
     # Add GUI in here that then calls run after quiting.
     def game_over(self):
         """This creates game over screen after the pygame event ends because they aren't able to work together to
         a seperate function is required to show this"""
         game_over_window = tk.Toplevel()
+        game_over_window.configure(bg="#1e1e2f")  # Light blue background
         game_over_window.title("Game Over")
 
 
-        window_width = 300
-        window_height = 200
+        window_width = 400
+        window_height = 250
         # Get the screen dimensions
         screen_width = game_over_window.winfo_screenwidth()
         screen_height = game_over_window.winfo_screenheight()
@@ -42,13 +79,40 @@ This is the setup for the screen
 
         # Set the geometry of the window
         game_over_window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+        game_over_window.resizable(False, False)
 
-        label = tk.Label(game_over_window, text="Gameover", font =("Comic Sans MS", 16)) #sets the text, font and size
-        label.pack(pady=20)
-        restart_button = tk.Button(game_over_window, text="Restart", command=lambda: [game_over_window.destroy(), self.restart()])
+        label = tk.Label(
+            game_over_window,
+            text="You Win!!!",           #The text displayed
+            font =("Comic Sans MS", 16), #The font and size
+            bg = "#1e1e2f",              #The background color
+            fg = "white",                #The text color
+            pady = 20
+        )
+        label.pack()
+
+        button_style = {
+            "font": ("Comic Sans MS", 16),
+            "bg": "#444",  # Button background
+            "fg": "#FFF",  # Text color
+            "activebackground": "#666",  # Background when hovered
+            "activeforeground": "#FFD700",  # Text color when hovered
+            "width": 12,
+            "relief": "raised",
+            "bd": 3
+        }
+
+        restart_button = tk.Button(
+            game_over_window,
+            text="Restart?",
+            command=lambda: [game_over_window.destroy(), self.restart()],
+            **button_style
+        )
         restart_button.pack(pady=5)
 
-        exit_button = tk.Button(game_over_window, text= "Exit", command=lambda: [game_over_window.destroy(), pygame.quit(), sys.exit()]) #Creates an exit button
+        exit_button = tk.Button(game_over_window, text= "Exit",
+                                command=lambda: [game_over_window.destroy(), pygame.quit(), sys.exit()],
+                                bg="red", fg="black") #Creates an exit button
         exit_button.pack(pady=10)
 
         game_over_window.mainloop()
@@ -73,7 +137,7 @@ This is the setup for the screen
                 if event.type == pygame.QUIT:
                     self.running = False
             self.badguy.npc_directions()
-            self.badguy.npcmovement()
+            self.badguy.npcmovement(self.walls)
             self.screen.fill('#9CBEBA')
             seconds = (pygame.time.get_ticks() - self.start_time) / 1000
             # self.screen.fill((60, 20, 30))
@@ -81,8 +145,9 @@ This is the setup for the screen
             self.screen.blit(timer_text, (0, 0))
 
             if not pygame.sprite.spritecollide(self.badguy, [self.guy], False):
-                self.guy.p1movement(pygame.key.get_pressed())
+                self.guy.p1movement(self.walls)
                 self.screen.fill('#9CBEBA')
+                self.walls.draw(self.screen)
                 self.screen.blit(self.guy.surf, self.guy.rect)
                 self.screen.blit(self.badguy.surf, self.badguy.rect)
 
@@ -102,11 +167,6 @@ This is the setup for the screen
             and it will display a message before restarting the game.
             """
 
-
-        pass
-
-    def maze(self):
-        """This would require a turtle to make the overall layout for the maze"""
 
         pass
 
